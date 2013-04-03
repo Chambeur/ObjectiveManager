d3ea9e9785f45fbf44c5a459863edf22a57b461f
Marc Chamberlin <marc.chamberlin@gmail.com>
2013-04-03 22:36:34 +0200

    Add label controller

--- app/controllers/labels_controller.rb
+++ app/controllers/labels_controller.rb
@@ -1,2 +1,31 @@
 class LabelsController < ApplicationController
+
+	def new 
+		@label = Label.new
+	end
+
+	def create
+		@label = Label.new(params[:label])
+		if @label.save
+			redirect_to labels_path
+		else
+			flash[:error] = "Error. Your label has not been saved."
+      render "index"
+		end
+	end
+
+	def index
+		@labels = Label.all
+	end
+
+	def show
+		@label = label.find(params[:id])
+	end
+
+	def destroy
+		@label = label.find(params[:id])
+		@label.destroy
+
+		redirect_to labels_path
+	end
 end
